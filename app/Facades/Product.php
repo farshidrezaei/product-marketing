8ee15facb05c22a272e264c0a3cc04501cf3f229
Farshid Rezaei <f.rezaei@vandar.io>
2023-08-01 12:00:41 +0330

    feat:add Facade for repositories for use decorator pattern to cache data

--- app/Facades/Product.php
+++ app/Facades/Product.php
@@ -3,13 +3,18 @@
 namespace App\Facades;
 
 use App\Contracts\ProductRepositoryContract;
+use App\Http\Filters\ProductVisitFilter;
+use App\Http\Requests\PaginatedProductVisitCountRequest;
+use App\Library\CacheHelper;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \App\Models\Product  show(int|string $productId);
- * @method static LengthAwarePaginator index(null|int $page = 1, null|int $perPage = 15):;
+ * @method static int                  countVisits(int|string $productId, ProductVisitFilter $filter);
+ * @method static LengthAwarePaginator index(null|int $page = 1, null|int $perPage = 15);
+ * @method static LengthAwarePaginator indexWithVisitsCount(PaginatedProductVisitCountRequest $request, ProductVisitFilter $filter, null|int $page = 1, null|int $perPage = 15);
  * @method static \App\Models\Product  store(array $attributes);
  * @method static \App\Models\Product  update(\App\Models\Product $product, array $attributes);
  * @method static bool                 destroy(\App\Models\Product $product);
@@ -18,6 +23,8 @@ use Illuminate\Support\Facades\Facade;
  */
 class Product extends Facade
 {
+
+
     protected static function getFacadeAccessor(): string
     {
         return ProductRepositoryContract::class;
@@ -41,5 +48,17 @@ class Product extends Facade
             fn() => self::__callStatic('index', $args)
         );
     }
+
+    public static function countVisitsCached(...$args): int
+    {
+        $cacheKey = CacheHelper::getHashedParameters($args[2]->validated());
+
+        return Cache::remember(
+            "products:$args[0]:visits_count:$cacheKey",
+            now()->addMinutes(15),
+            fn() => self::__callStatic('countVisits', $args)
+        );
+    }
+
 
 }
